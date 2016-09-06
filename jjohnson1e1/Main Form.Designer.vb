dafcc00af5be8f1ef1429d88168ef4c98e7df955
Jason Johnson <jason.johnson@my.southeastmn.edu>
2016-09-06 14:21:26 -0500

    Commit jjohnson1e1 - Shiloh Bonus Calculator. Calculates 1% bonus. Print button removes buttons from view and presents for printing.

---
+++ jjohnson1e1/Main Form.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBonus
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmBonus))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblBonus = New System.Windows.Forms.Label()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        Me.txtAnnualSales = New System.Windows.Forms.TextBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.PrintForm1 = New Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(84, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(179, 30)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Bonus Calculator"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(21, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "&ID:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(121, 101)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Annual &sales:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(233, 101)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Bonus:"
+        '
+        'lblBonus
+        '
+        Me.lblBonus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblBonus.Location = New System.Drawing.Point(236, 114)
+        Me.lblBonus.Name = "lblBonus"
+        Me.lblBonus.Size = New System.Drawing.Size(100, 23)
+        Me.lblBonus.TabIndex = 5
+        Me.lblBonus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtId
+        '
+        Me.txtId.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.txtId.Location = New System.Drawing.Point(12, 117)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(100, 20)
+        Me.txtId.TabIndex = 1
+        '
+        'txtAnnualSales
+        '
+        Me.txtAnnualSales.Location = New System.Drawing.Point(124, 117)
+        Me.txtAnnualSales.Name = "txtAnnualSales"
+        Me.txtAnnualSales.Size = New System.Drawing.Size(100, 20)
+        Me.txtAnnualSales.TabIndex = 3
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(12, 178)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(79, 23)
+        Me.btnCalc.TabIndex = 7
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(98, 178)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(79, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "&Print"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(184, 178)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(79, 23)
+        Me.btnClear.TabIndex = 9
+        Me.btnClear.Text = "C&lear Screen"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(270, 178)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(79, 23)
+        Me.btnExit.TabIndex = 10
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'PrintForm1
+        '
+        Me.PrintForm1.DocumentName = "document"
+        Me.PrintForm1.Form = Me
+        Me.PrintForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPrinter
+        Me.PrintForm1.PrinterSettings = CType(resources.GetObject("PrintForm1.PrinterSettings"), System.Drawing.Printing.PrinterSettings)
+        Me.PrintForm1.PrintFileName = Nothing
+        '
+        'frmBonus
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(372, 231)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.txtAnnualSales)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.lblBonus)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "frmBonus"
+        Me.Text = "JJohnson1e1 Shiloh Products "
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblBonus As Label
+    Friend WithEvents txtId As TextBox
+    Friend WithEvents txtAnnualSales As TextBox
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents PrintForm1 As PowerPacks.Printing.PrintForm
+End Class
---
+++ jjohnson1e1/Main Form.vb
@@ -0,0 +1,38 @@
+﻿' Name: Shiloh Products Bonus Form  
+' Purpose: Calculate employee bonus at 1%.
+'
+' Programmer: Jason R. Johnson on 9/6/29/2016
+
+Public Class frmBonus
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close form
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Clear screen and set focus to ID field.
+        txtId.Text = String.Empty
+        txtAnnualSales.Text = String.Empty
+        lblBonus.Text = String.Empty
+        txtId.Focus()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Set print preview to remove button view for print.
+        Me.Height = Me.Height - 75
+        PrintForm1.PrintAction = Printing.PrintAction.PrintToPreview
+        PrintForm1.Print()
+        Me.Height = Me.Height + 75
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' Calculate bonus at 1%. 
+        ' lblBonus.Text = txtAnnualSales.Text * 0.01
+        ' lblBonus.Text = Val(txtAnnualSales.Text) * 0.01
+        lblBonus.Text = Format(Val(txtAnnualSales.Text) * 0.01, "Currency")
+    End Sub
+
+    Private Sub txtAnnualSales_TextChanged(sender As Object, e As EventArgs) Handles txtAnnualSales.TextChanged
+
+    End Sub
+End Class
